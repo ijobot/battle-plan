1c063ff4e7e1288338f076be6dd35d17e9c0cd75
Joe Loftus <joe@ijobot.tech>
2024-07-21 00:05:41 +0100

    added AngularMaterials, updated inputs, some CSS changes

--- battle-plan-app/src/app/components/combatant-row/combatant-row.component.ts
+++ battle-plan-app/src/app/components/combatant-row/combatant-row.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { Combatant } from '../../models/combatant';
 import { CombatantService } from '../../services/combatant.service';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-combatant-row',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatIconModule],
   templateUrl: './combatant-row.component.html',
   styleUrl: './combatant-row.component.scss',
 })
