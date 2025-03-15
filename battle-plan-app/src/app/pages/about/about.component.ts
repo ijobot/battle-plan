2e6476015f4ce6587b15b2de63146707bbd11642
Joe Loftus <josephwloftus2@gmail.com>
2025-03-15 01:18:20 +0000

    fixed standalone true issue with newest CLI update

--- battle-plan-app/src/app/pages/about/about.component.ts
+++ battle-plan-app/src/app/pages/about/about.component.ts
@@ -2,10 +2,9 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 
 @Component({
-  selector: 'app-about',
-  standalone: true,
-  imports: [NavbarComponent, NavbarComponent],
-  templateUrl: './about.component.html',
-  styleUrl: './about.component.scss',
+    selector: 'app-about',
+    imports: [NavbarComponent, NavbarComponent],
+    templateUrl: './about.component.html',
+    styleUrl: './about.component.scss'
 })
 export class AboutComponent {}
--- battle-plan-app/src/app/pages/home/home.component.ts
+++ battle-plan-app/src/app/pages/home/home.component.ts
@@ -2,10 +2,9 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [NavbarComponent],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.scss',
+    selector: 'app-home',
+    imports: [NavbarComponent],
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.scss'
 })
 export class HomeComponent {}
